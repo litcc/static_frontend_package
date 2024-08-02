f4fdebe623250f42bf2bade9662ca23c2fe38ed2
litcc <i@litcc.com>
2024-08-03 00:34:12 +0800

    fix: update

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@ use anyhow::Result;
 use crate::web_service::ServerConfig;
 
 #[tokio::main]
-async fn main() ->Result<()>{
+async fn main() -> Result<()> {
     log::init()?;
-    let web = web_service::WebService::new(ServerConfig{
-        port:8080,
-        host:"0.0.0.0".to_owned(),
+    let port = std::env::var("PORT").map(|i| {
+        if i.is_empty() {
+            8080
+        } else {
+            i.parse::<u16>().unwrap_or(8080)
+        }
+    }).unwrap_or(8080);
+
+    let web = web_service::WebService::new(ServerConfig {
+        port,
+        host: "0.0.0.0".to_owned(),
         timeout: 0,
     }).await;
 
