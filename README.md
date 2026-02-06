e2347131698521bb02c7cd9fc1915cc23c3ce979
litcc <i@litcc.com>
2026-02-06 23:50:08 +0800

    fix: update dependencies, adjust allocator for target environments, and enhance Docker configurations

--- README.md
+++ README.md
@@ -16,6 +16,16 @@ docker build . -t <自定义>:<自定义>
 
 
 
+
 # 引入方式
 
 前端项目打包,使用docker 基于本镜像将静态资源打包成镜像
+
+
+
+## 构建
+
+```shell
+RUSTC_WRAPPER="" cross build --target x86_64-unknown-linux-musl 
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
-pub(crate) mod web_service;
 pub(crate) mod log;
+pub(crate) mod web_service;
 
-use anyhow::Result;
 use crate::web_service::ServerConfig;
+use anyhow::Result;
+
+/// 当 MSVC 环境下使用 mimalloc
+#[cfg(target_env = "msvc")]
+pub use mimalloc::MiMalloc as Allocator;
+/// 当非 MSVC 环境下使用 jemalloc
+#[cfg(not(target_env = "msvc"))]
+pub use tikv_jemallocator::Jemalloc as Allocator;
+
+#[global_allocator]
+static GLOBAL: Allocator = Allocator;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     log::init()?;
-    let port = std::env::var("PORT").map(|i| {
-        if i.is_empty() {
-            8080
-        } else {
-            i.parse::<u16>().unwrap_or(8080)
-        }
-    }).unwrap_or(8080);
+    let port = std::env::var("PORT")
+        .map(|i| {
+            if i.is_empty() {
+                8080
+            } else {
+                i.parse::<u16>().unwrap_or(8080)
+            }
+        })
+        .unwrap_or(8080);
 
     let web = web_service::WebService::new(ServerConfig {
         port,
         host: "0.0.0.0".to_owned(),
         timeout: 0,
-    }).await;
+    })
+    .await;
 
     web.await_web_task().await;
     Ok(())
